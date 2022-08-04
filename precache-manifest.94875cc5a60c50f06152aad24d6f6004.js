20814710c1cfe0432431a74af5f4de436d360a60
Code Sandbox <hello@codesandbox.io>
2022-08-04 20:52:44 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.94875cc5a60c50f06152aad24d6f6004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046c11a032a8607d2ad559a9eee64040",
+    "url": "https://9opsec.github.io/csb-tw25g7/index.html"
+  },
+  {
+    "revision": "4e55da79c04d2e8f965a",
+    "url": "https://9opsec.github.io/csb-tw25g7/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "0c860124e95bbbf29789",
+    "url": "https://9opsec.github.io/csb-tw25g7/static/js/2.1d675659.chunk.js"
+  },
+  {
+    "revision": "2b1bf4c5f6156582acdb3d33a1b72617",
+    "url": "https://9opsec.github.io/csb-tw25g7/static/js/2.1d675659.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e55da79c04d2e8f965a",
+    "url": "https://9opsec.github.io/csb-tw25g7/static/js/main.9540d978.chunk.js"
+  },
+  {
+    "revision": "1775c093973e0b024ce6",
+    "url": "https://9opsec.github.io/csb-tw25g7/static/js/runtime-main.9baf707b.js"
+  }
+]);
